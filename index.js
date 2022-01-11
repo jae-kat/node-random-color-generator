afdba7f957f40ea1a706bbf9b5053bbe3a6d4284
katharina jäger <jaeger.kt@gmail.com>
2022-01-11 17:43:14 +0100

    Add last line readme, space after // in comments

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-//changes terminal color
+// changes terminal color
 import kuler from 'kuler';
 // random hex color generator
 import { generate } from 'random-hex';
-//this converts a color to a random variation of itself
+// this converts a color to a random variation of itself
 import randomized from 'randomized-colors';
-//this converts rgb to hex
+// this converts rgb to hex
 import rgb2hex from 'rgb2hex';
-//makes color darker or lighter
+// makes color darker or lighter
 import shader from 'shader';
 
-//this logs a random hex value
+// this logs a random hex value
 const randomHex = generate();
 const hash1 = `################################
 ################################
@@ -22,17 +22,17 @@ const hash2 = `          ####
 ################################
 ################################`;
 
-//stores user input in a variable
+// stores user input in a variable
 const colorInput = process.argv[2];
 const hueInput = process.argv[3];
 
-//this converts a color name input to a hue variation
+// this converts a color name input to a hue variation
 const hue = colorInput ? randomized(colorInput, 0.3) : 'rgb(0, 0, 0)';
 
-//this converts the rgb value of the hue variation to hex
+// this converts the rgb value of the hue variation to hex
 const hex = rgb2hex(hue).hex;
 
-//this creates darker or lighter instances of a color
+// this creates darker or lighter instances of a color
 const darkerShade = shader(hex, -0.5);
 const lighterShade = shader(hex, 0.5);
 
--- readme.md
+++ readme.md
@@ -2,11 +2,13 @@
 
 > return a random color in the command line.
 
-enter a specific command in the command line to generate a block of approximately 31x9 # characters in a random color, and display the random color's name in the middle (using hex code, e.g. #ff0000)
+enter a specific command (node index.js) in the command line to generate a block of approximately 31x9 # characters in a random color, and display the random color's name in the middle (using hex code, e.g. #ff0000)
 
 It also accepts the request for a:
 
 - hue (e.g. green or red or blue)
-- luminosity (e.g. light or dark)
+- luminosity (light or dark)
 
 ...and then generates random colors that match those choices.
+
+enter the command, then the hue, finally the desired luminosity
